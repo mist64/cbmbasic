af53b9e8a2803543c842ddb900f1e4fb9146b6cb
Adam Gates <adamgates@fastmail.fm>
2010-07-04 13:02:09 +0000

    Fix up kernal_quote mode output.

--- runtime.c
+++ runtime.c
@@ -454,90 +454,94 @@ printf("CHROUT: %d @ %x,%x,%x,%x\n", A, a, b, c, d);
         } else
             C = 0;
     } else {
-		switch (A) {
-			case 5:
-				set_color(COLOR_WHITE);
-				break;
-			case 10:
-				kernal_quote = 0;
-				break;
-			case 13:
-				kernal_quote = 0;
-				putchar(13);
-				putchar(10);
-				break;
-			case 17: /* CSR DOWN */
-				down_cursor();
-				break;
-			case 19: /* CSR HOME */
-				move_cursor(0, 0);
-				break;
-			case 28:
-				set_color(COLOR_RED);
-				break;
-			case 29: /* CSR RIGHT */
-				right_cursor();
-				break;
-			case 30:
-				set_color(COLOR_GREEN);
-				break;
-			case 31:
-				set_color(COLOR_BLUE);
-				break;
-			case 129:
-				set_color(COLOR_ORANGE);
-				break;
-			case 144:
-				set_color(COLOR_BLACK);
-				break;
-			case 145: /* CSR UP */
-				up_cursor();
-				break;
-			case 147: /* clear screen */
+        if (kernal_quote) {
+            if (A == '"') kernal_quote = 0;
+            putchar(A);
+        } else {
+            switch (A) {
+                case 5:
+                    set_color(COLOR_WHITE);
+                    break;
+                case 10:
+                    kernal_quote = 0;
+                    break;
+                case 13:
+                    kernal_quote = 0;
+                    putchar(13);
+                    putchar(10);
+                    break;
+                case 17: /* CSR DOWN */
+                    down_cursor();
+                    break;
+                case 19: /* CSR HOME */
+                    move_cursor(0, 0);
+                    break;
+                case 28:
+                    set_color(COLOR_RED);
+                    break;
+                case 29: /* CSR RIGHT */
+                    right_cursor();
+                    break;
+                case 30:
+                    set_color(COLOR_GREEN);
+                    break;
+                case 31:
+                    set_color(COLOR_BLUE);
+                    break;
+                case 129:
+                    set_color(COLOR_ORANGE);
+                    break;
+                case 144:
+                    set_color(COLOR_BLACK);
+                    break;
+                case 145: /* CSR UP */
+                    up_cursor();
+                    break;
+                case 147: /* clear screen */
 #ifndef NO_CLRHOME
-				if (!kernal_quote)
-					clear_screen();
+                    clear_screen();
 #endif
-				break;
-			case 149:
-				set_color(COLOR_BROWN);
-				break;
-			case 150:
-				set_color(COLOR_LTRED);
-				break;
-			case 151:
-				set_color(COLOR_GREY1);
-				break;
-			case 152:
-				set_color(COLOR_GREY2);
-				break;
-			case 153:
-				set_color(COLOR_LTGREEN);
-				break;
-			case 154:
-				set_color(COLOR_LTBLUE);
-				break;
-			case 155:
-				set_color(COLOR_GREY3);
-				break;
-			case 156:
-				set_color(COLOR_PURPLE);
-				break;
-			case 158:
-				set_color(COLOR_YELLOW);
-				break;
-			case 159:
-				set_color(COLOR_CYAN);
-				break;
-			case 157: /* CSR LEFT */
-				left_cursor();
-				break;
-			case '"':
-				kernal_quote = 1-kernal_quote;
-				// fallthrough
-			default:
-				putchar(A);
-		}
+                    break;
+                case 149:
+                    set_color(COLOR_BROWN);
+                    break;
+                case 150:
+                    set_color(COLOR_LTRED);
+                    break;
+                case 151:
+                    set_color(COLOR_GREY1);
+                    break;
+                case 152:
+                    set_color(COLOR_GREY2);
+                    break;
+                case 153:
+                    set_color(COLOR_LTGREEN);
+                    break;
+                case 154:
+                    set_color(COLOR_LTBLUE);
+                    break;
+                case 155:
+                    set_color(COLOR_GREY3);
+                    break;
+                case 156:
+                    set_color(COLOR_PURPLE);
+                    break;
+                case 158:
+                    set_color(COLOR_YELLOW);
+                    break;
+                case 159:
+                    set_color(COLOR_CYAN);
+                    break;
+                case 157: /* CSR LEFT */
+                    left_cursor();
+                    break;
+                case '"':
+                    kernal_quote = 1;
+                    // fallthrough
+                default:
+                    putchar(A);
+            }
+        }
 #endif
 		fflush(stdout);
 		C = 0;
